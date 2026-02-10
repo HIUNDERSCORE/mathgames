45d9edb396617865f74541ff7c1826b61bf2ff85
HIUNDERSCORE <128417733+HIUNDERSCORE@users.noreply.github.com>
2026-02-09 18:23:40 -0800

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -10,9 +10,7 @@ import { createBareServer } from '@tomphttp/bare-server-node';
 
 const app = express();
 const bare = createBareServer('/bare/');
-const server = createServer();
-app.get('/health', (req, res) => res.status(200).send('OK'));
-
+const server = createServer()
 app.use(express.static('public'));
 app.use('/uv/', express.static(uvPath));
 
